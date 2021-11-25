b77a4cbd6030fc0514708ba47e11f2af4a007539
Valentin Bousquet <bousquet.valentin@gmail.com>
2021-11-25 15:23:32 +0100

    Add all scss files

--- src/Pages/Works/index.jsx
+++ src/Pages/Works/index.jsx
@@ -1,15 +1,16 @@
 import React, { useContext } from 'react';
 import DataContext from '../../config/dataContext';
 import { v4 as uuidv4 } from 'uuid';
+import './works.scss';
 
 const Index = () => {
   const data = useContext(DataContext);
-  const works = data.works
+  // const works = data.works
 
   return (
     <div className="container">
       <h1>WORKS—INDEX</h1>
-        {
+        {/* {
           works
           .map((work) => (
             <div
@@ -18,7 +19,7 @@ const Index = () => {
               {work.strMeal}
             </div>
           ))
-        }
+        } */}
     </div>
   );
 };
