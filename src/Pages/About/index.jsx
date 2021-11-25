1b9fb189d7356fb3951f22353f13bfcaa1b54659
Valentin Bousquet <bousquet.valentin@gmail.com>
2021-11-25 17:08:48 +0100

    Fonts px done

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -6,7 +6,7 @@ const About = () => {
 
   return (
     <>
-      <CustomFont tag="h1" className="big__font" content="MpeldqxaOS" />
+      <CustomFont tag="h1" className="big__font" content="Model×Studio" />
       <CustomFont tag="h3" className="medium__font" content="MpeldqxaOS" />
       <CustomFont tag="p" className="small__font" content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti repellendus impedit maxime accusamus? Quam veritatis officiis ullam sequi voluptatem ipsa, beatae recusandae minima placeat perferendis dolor in esse, ratione eos." />
     </>
